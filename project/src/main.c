2644f3206d772edc55ff464375599dfe41d7bd80
Vishal Kotak <vishalkt07@gmail.com>
2024-06-16 22:12:55 -0400

    First version of database headers.

--- project/src/main.c
+++ project/src/main.c
@@ -2,6 +2,9 @@
 #include <stdbool.h>
 #include <getopt.h>
 
+#include "common.h"
+#include "parse.h"
+
 void print_usage(char *argv[]) {
 	printf("Usage %s -n -f <database file>\n", argv[0]);
 	printf("\t -n - create database file\n");
@@ -36,6 +39,22 @@ int main(int argc, char *argv[]) {
 		print_usage(argv);
 		return 0;
 	}
+
+	if (newfile) {
+		dbfd = create_db_file(filepath);
+		if (dfdb == STATUS_ERROR) {
+			printf("Unable to create database file\n");
+			return -1;
+		}
+		create_db_header(dbfd, &dbhdr);
+	} else {
+		dbfd = open_db_file(filepath);
+		if (dfdb == STATUS_ERROR) {
+			printf("Unable to open database file\n");
+			return -1;
+		}
+	}
+
 
 	printf("Newfile: %d\n", newfile);
 	printf("Filepath: %s\n", filepath);
---
+++ project/src/parse.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+
+#include "parse.h"
+#include "common.h"
+
+int create_db_header(int fd, struct dbheader_t **headerOut) {
+	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+	if (header == -1) {
+		printf("Malloc failed to create db header\n");
+		return STATUS_ERROR;
+	}
+
+	header->version = 0x1;
+	header->count = 0;
+	header->magic = HEADER_MAGIC;
+	header->filesize = sizeof(struct dbheader_t);
+	
+	*headerOut = header;
+	return STATUS_SUCCESS;
+}
